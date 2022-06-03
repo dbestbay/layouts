4201f980c7757178a4e077671f9fab528b7bc9c4
Bayu Prayoga <106808389+dbestbay@users.noreply.github.com>
2022-06-03 20:11:57 +0700

    Update and rename Layouts.php to Layouts.blade.php

--- Layouts.blade.php
+++ Layouts.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tutorial CRUD Laravel 8 untuk Pemula</title>
+    <title>Data Warga</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
